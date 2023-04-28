62edc83f541871442cdc3826c28dbd3befbc8bdb
guzal <220930@astanait.edu.kz>
2023-04-28 18:50:08 +0600

    4.There was created a constructor for the class and initialize the instance variable "list" inside MyLinkedListStack() by using "new" operator.

--- MyLinkedListStack.java
+++ MyLinkedListStack.java
@@ -2,7 +2,7 @@ import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
     //declaretion of instance variable "list"
     private MyLinkedList<T> list;
-
+    //Initialize the instance variable "list"
     public MyLinkedListStack() {
         list = new MyLinkedList<>();
 
