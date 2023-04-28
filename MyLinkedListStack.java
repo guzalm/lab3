0fa5a20b4d48661d43e65a12ae01faa00ae1525f
guzal <220930@astanait.edu.kz>
2023-04-28 18:45:14 +0600

    3.I declared an instance variable "list"

--- MyLinkedListStack.java
+++ MyLinkedListStack.java
@@ -1,8 +1,11 @@
 import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
+    //declaretion of instance variable "list"
     private MyLinkedList<T> list;
 
     public MyLinkedListStack() {
         list = new MyLinkedList<>();
 
+    }
 }
+
