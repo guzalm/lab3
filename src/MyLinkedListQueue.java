c40f23c07b0eec1d5c948bf3c71a67fe0b932626
guzal <220930@astanait.edu.kz>
2023-04-29 13:39:46 +0600

    STEP 15: Created a constructor for the class and initialize the instance variable "list" by using new.

--- src/MyLinkedListQueue.java
+++ src/MyLinkedListQueue.java
@@ -2,4 +2,8 @@ import java.util.NoSuchElementException;
 public class MyLinkedListQueue<T> {
     // Declaration of instance variable "list"
     private MyLinkedList<T> list;
+    // Initialize the instance variable "list"
+    public MyLinkedListQueue() {
+        list = new MyLinkedList<>();
+    }
 }
