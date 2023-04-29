ee2ad334346a4533288a44a3a4ac48988d17e2c3
guzal <220930@astanait.edu.kz>
2023-04-29 13:23:01 +0600

    STEP 9: I added the peek method that returns the top element of the stack without removing it using the getFirst method for MyLinkedList. There is exception which works if stack is empty, If not, it calls getFirst() method.

---
+++ src/MyLinkedList.java
@@ -0,0 +1,57 @@
+import java.util.NoSuchElementException;
+
+public class MyLinkedList<T> {
+    private Node<T> head;
+    private int size;
+
+    private static class Node<T> {
+        private T data;
+        private Node<T> next;
+
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+    public MyLinkedList() {
+        head = null;
+        size = 0;
+    }
+
+    // Adds an element to the beginning of the list
+    public void addFirst(T element) {
+        Node<T> newNode = new Node<>(element);
+        newNode.next = head;
+        head = newNode;
+        size++;
+    }
+
+    // Removes and returns the first element of the list
+    public T removeFirst() throws NoSuchElementException {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        T data = head.data;
+        head = head.next;
+        size--;
+        return data;
+    }
+
+    // Returns the first element of the list
+    public T getFirst() throws NoSuchElementException {
+        if (head == null) {
+            throw new NoSuchElementException();
+        }
+        return head.data;
+    }
+
+    // Returns the size of the list
+    public int size() {
+        return size;
+    }
+
+    // Returns true if the list is empty, false otherwise
+    public boolean isEmpty() {
+        return head == null;
+    }
+}
--- src/MyLinkedListStack.java
+++ src/MyLinkedListStack.java
@@ -1,6 +1,6 @@
 import java.util.EmptyStackException;
 public class MyLinkedListStack<T> {
-    //declaretion of instance variable "list"
+    //declaration of instance variable "list"
     private MyLinkedList<T> list;
     //Initialize the instance variable "list"
     public MyLinkedListStack() {
