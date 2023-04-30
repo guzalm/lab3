674f51787f2ca0fdecc09adffefc204163d1dd18
guzal <220930@astanait.edu.kz>
2023-04-30 21:26:56 +0600

    STEP 23: In main I started checking methods of MyLinkedListQueue class. First one is "isEmpty" method. When we check is queue isEmpty, system prints "true" message, because we did not add anything to our queue.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         MyLinkedListQueue<Double> queue = new MyLinkedListQueue<>();
+        //Start checking methods
+        System.out.println(queue.isEmpty());//prints "true"
     }
 }
